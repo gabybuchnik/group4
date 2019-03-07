fad87e5db1405b25ecdcbddcf38cb5151e7182b3
gabybuchnik <44199356+gabybuchnik@users.noreply.github.com>
2019-03-07 15:43:46 +0200

    answer5ajs

---
+++ modules/04 Advanced Javascript/Assignments/5/app.js
@@ -0,0 +1,71 @@
+var startBtn = document.getElementById("timer-button-start");
+var stopBtn = document.getElementById("timer-button-stop");
+var resetBtn = document.getElementById("timer-button-reset");
+var countdown = document.getElementById("countdown");
+var startFrom = document.getElementById("start-from");
+var timer = document.getElementById("timer");
+
+var Counter = (function () {
+    var m = 0 , s = 0;
+    var interval;
+    function start() {
+        clearInterval(interval);
+        if (countdown.checked && startFrom.value) {
+            interval = setInterval(startCountDown, 1000);
+        }
+        else {
+            interval = setInterval(startTimer, 1000);
+        }
+    }
+    function stop() {
+        clearInterval(interval);
+    }
+    function reset() {
+        clearInterval(interval);
+        s = m = 0;
+        setPrefix();
+        document.getElementById("counter").innerHTML = m + ":" + s;
+    }
+    function startTimer() {
+        setPrefix();
+        document.getElementById("counter").innerHTML = m + ":" + s;
+        s++;
+        if (s === 60) {
+            m++;
+            s = 0;
+        }
+    }
+    function startCountDown() {
+        if (s <= 0) {
+            m--;
+            s = 60;
+        }
+        s--;
+        setPrefix();
+        document.getElementById("counter").innerHTML = m + ":" + s;
+    }
+    function setPrefix() {
+        s <= 9 ? s = ("0" + s).slice(-2) : s;
+        m <= 9 ? m = ("0" + m).slice(-2) : m;
+    }
+    return {
+        start: start,
+        stop: stop,
+        reset: reset
+    }
+})();
+
+function setDisable() {
+    startFrom.setAttribute("disabled", "disabled");
+}
+function removeDisable() {
+    startFrom.removeAttribute("disabled");
+}
+
+startBtn.onclick = Counter.start;
+stopBtn.onclick = Counter.stop;
+resetBtn.onclick = Counter.reset;
+timer.onchange = setDisable;
+countdown.onchange = removeDisable
+
+
