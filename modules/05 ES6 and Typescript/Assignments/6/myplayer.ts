7279ec7859f89e6a51b29ee50680c4b5ab2ac924
gabybuchnik <44199356+gabybuchnik@users.noreply.github.com>
2019-03-25 13:59:33 +0200

    fix

--- modules/05 ES6 and Typescript/Assignments/6/myplayer.ts
+++ modules/05 ES6 and Typescript/Assignments/6/myplayer.ts
@@ -1,9 +1,9 @@
-export interface Iplay {
+interface Iplay {
     play(song: string): void;
     next(): void;
     prev(): void;
 }
-export class Musician implements Iplay {
+class Musician implements Iplay {
     constructor(public name: string, public age: number, public instruments: instrument) {
         this.name = name;
         this.age = age;
@@ -31,7 +31,7 @@ export class Musician implements Iplay {
         console.log(`musician ${this.name} is now playing ${songs[index]}`);
     }
 }
-export class Band implements Iplay {
+class Band implements Iplay {
     constructor(public name: string, public musician: Array<Musician>) {
         this.name = name;
         this.musician = musician;
@@ -58,7 +58,7 @@ export class Band implements Iplay {
         console.log(`band ${this.name} is now playing ${songs[index]}`);
     }
 }
-export class Radio implements Iplay {
+class Radio implements Iplay {
     constructor(public brand: string, public color: color, public price: number) {
         this.brand = brand;
         this.color = color;
@@ -86,34 +86,33 @@ export class Radio implements Iplay {
         console.log(`radio ${this.brand} is now playing ${songs[index]}`);
     }
 }
-export enum instrument {
+enum instrument {
     Guitar = "Guitar",
     Drums = "Drums",
     Bass = "Bass",
     None = "None"
 }
-export enum color {
+enum color {
     red = "red",
     blue = "blue",
     yellow = "yellow",
 }
 let songs: Array<string> = ["song1", "song2", "song3", "song4",], index = 0;
 
-import * as module from "./myplayer";
-let radio1: module.Radio = new module.Radio("poineer", color.blue, 350);
+let radio1: Radio = new Radio("poineer", color.blue, 350);
 radio1.play(songs[index]);
 radio1.next();
 radio1.next();
 radio1.prev();
-let musician1: module.Musician = new module.Musician("gaby", 30, instrument.Guitar);
-let musician2: module.Musician = new module.Musician("yosi", 40, instrument.Drums);
-let musician3: module.Musician = new module.Musician("maly", 37, instrument.None);
-let musician4: module.Musician = new module.Musician("nissan", 46, instrument.Bass);
+let musician1: Musician = new Musician("gaby", 30, instrument.Guitar);
+let musician2: Musician = new Musician("yosi", 40, instrument.Drums);
+let musician3: Musician = new Musician("maly", 37, instrument.None);
+let musician4: Musician = new Musician("nissan", 46, instrument.Bass);
 musician1.play(songs[index]);
 musician2.prev();
 musician3.next();
 musician4.play(songs[index]);
-let band1: module.Band = new module.Band("bandRock", [musician1, musician2, musician3, musician4]);
+let band1: Band = new Band("bandRock", [musician1, musician2, musician3, musician4]);
 band1.play(songs[index]);
 band1.prev();
 band1.next();
